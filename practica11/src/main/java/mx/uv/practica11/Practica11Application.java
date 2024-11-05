41ab720cec2e73ff18d863ed19925e83f94a5890
VEALASCO <velascoale83@gmail.com>
2024-11-05 16:56:24 -0600

    practica11

--- practica11/src/main/java/mx/uv/practica11/Practica11Application.java
+++ practica11/src/main/java/mx/uv/practica11/Practica11Application.java
@@ -2,6 +2,8 @@ package mx.uv.practica11;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,27 +12,39 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class Practica11Application {
 
+	Color color = new Color();
+
 	public static void main(String[] args) {
 		SpringApplication.run(Practica11Application.class, args);
 	}
 
-
 	//Api relativo a los colores
 
-	@RequestMapping (method = RequestMethod.PUT, value = "/color")
-	String cambiarColor(){
-		return"Color cambiado";
+	//para llamar al método
+	// curl -X PUT -d @color.json -H "Content-Type: application/json" http://localhost:8080/color
+	@RequestMapping (method = RequestMethod.PUT, value = "/color")	
+	String cambiarColor(@RequestBody Color objeto) {
+		System.out.println(objeto);
+		color = objeto;
+		return "color cambiado";
 	}
-	@RequestMapping (method = RequestMethod.DELETE, value = "/color")
-	String eliminarColor(){
-		return"Color eliminado";
+	@RequestMapping (method = RequestMethod.DELETE, value = "/color/{id}")
+	Color eliminarColor(@PathVariable String id) {
+		System.out.println(id);
+		color = null;
+		return color;
 	}
 	@RequestMapping (method = RequestMethod.GET, value = "/color")
-	String listarColores(){
-		 return"Color listado";
+	Color listarColor() {
+		return color;
 	}
 	@RequestMapping (method = RequestMethod.POST, value = "/color")
-	String crearColor(){
-		return"Color Creado";
+	String crearColores() {
+		color.setId("1");
+		color.setNombreColor("blanco");
+		color.setValorColor("255");
+		System.out.println(color.getNombreColor());
+		return "color creado";
 	}
+
 }
